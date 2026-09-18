df204328b3664875f6f035f3943e412cac7c7b9c
FortCollinsBarber-netizen <FortCollinsBarber@icloud.com>
2026-09-18 12:36:42 -0600

    Make completed macOS releases filesystem read-only

--- deploy/macos/install_company_host.sh
+++ deploy/macos/install_company_host.sh
@@ -51,6 +51,7 @@ else
   "$VENV/bin/python" -m pip install --upgrade pip
   "$VENV/bin/python" -m pip install "$CORE"
   touch "$RELEASE/.ready"
+  chmod -R a-w "$RELEASE"
 fi
 
 VERSION="$("$PYTHON_BIN" - "$CORE" <<'PY'
